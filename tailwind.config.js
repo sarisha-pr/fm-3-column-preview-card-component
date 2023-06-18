eee08757d54be7e0758cc992f64d8b7a8dcba95d
Sarisha <94773351+sarisha-pr@users.noreply.github.com>
2023-06-18 11:05:56 +0800

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,117 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{html,js}',
+  ],
+  theme: {
+    screens:{
+      xs:'390px',
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px',
+    },
+
+    colors: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'orange': 'hsl(31, 77%, 52%)',
+      'cyan': 'hsl(184, 100%, 22%)',
+      'dark-cyan': 'hsl(179, 100%, 13%)',
+      'transparent-white': 'hsla(0, 0%, 100%, 0.75)',
+      'light-gray': 'hsl(0, 0%, 95%)',
+    },
+
+   /**
+   * Setup font-family
+   */
+    fontFamily: {
+      'shoulders': ['Big Shoulders Display', 'cursive'],
+      'lexend': ['Lexend Deca', 'sans-serif'],
+    },
+    extend: {
+      /**
+       * Create custom text-shadow
+       * Reference: https://www.hyperui.dev/blog/text-shadow-with-tailwindcss
+       */
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
+  },
+
+  // Do not include tailwind default container
+  corePlugins: {
+    container: false
+  },
+  
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        /**
+         * Create custom container
+         * Reference: https://stefvanlooveren.me/blog/custom-container-width-tailwind-css
+         */
+        '.container': {
+          width: '100%',
+          paddingLeft: '15px',
+          paddingRight: '15px',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+
+          '@screen sm': {
+            maxWidth: '540px',
+            paddingLeft: '0',
+            paddingRight: '0',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1152px',
+          },
+        },
+
+        /**
+         * Create custom writing mode
+         */
+        '.wm-vertical-rl': {
+          writingMode: 'vertical-rl',
+        },
+        '.wm-horizontal-tb': {
+          writingMode: 'horizontal-tb',
+        },
+
+        /**
+         * Image scale
+         */
+        '.img-scale': {
+          transform: 'scale(1.1)',
+        },
+      })
+    },
+
+    /**
+     * Create custom text-shadow
+     * Reference: https://www.hyperui.dev/blog/text-shadow-with-tailwindcss
+     */
+    function ({matchUtilities, theme}) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    },
+  ],
+
+  safelist: [
+  ],
+}
